db76213a0027f8cbb896c9af1543321729a487fd
ikaz <icastillejos@gmail.com>
2014-08-09 14:50:18 -0700

    Added tests

---
+++ Flask/ex2/flaskr/flaskr_tests.py
@@ -0,0 +1,45 @@
+import os
+import flaskr
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+	def setUp(self):
+		self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+		flaskr.app.config['TESTING']=True
+		self.app =flaskr.app.test_client()
+		flaskr.init_db()
+
+	def tearDown(self):
+		os.close(self.db_fd)
+		os.unlink(flaskr.app.config['DATABASE'])
+
+	def login(self, username, password):
+		return self.app.post('/login', data=dict(
+			username=username,
+			password=password
+			), follow_redirects=True)
+
+	def logout(self):
+		return self.app.get('/logout', follow_redirects=True)
+
+	## START TESTS ###
+	def test_empty_db(self):
+		rv = self.app.get('/')
+		assert 'No entries so far' in rv.data
+
+	def test_login_logout(self):
+		rv = self.login('admin', 'default')
+		assert 'You were logged in' in rv.data
+		rv =  self.logout()
+		assert 'You were logged out' in rv.data
+		rv = self.login('adminfake', 'default')
+		assert 'Invalid username' in rv.data
+		rv = self.login('admin', 'fakepassword')
+		assert 'Invalid password' in rv.data
+
+	### END TESTS ###
+
+if __name__ == '__main__':
+	unittest.main()
+
